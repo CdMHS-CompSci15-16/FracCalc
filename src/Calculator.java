b7501f5a2bad56dd0efcb7ae31f96f6e729a1c01
NotRyan <ryanchan989@gmail.com>
2015-09-28 10:00:04 -0700

    Comments_Updated

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,54 +1,54 @@
 //By Ryan Chan
 //AP Computer Science 2015
-public class Calculator {//Class for performing mathematical operations to fractions
-	Frac add(Frac f1, Frac f2){//Addition class
-		int[] info=new int[3];//Creates an array in order to receive data from the matchDenominator method
-		System.arraycopy(matchDenominator(f1,f2),0,info,0,3);//Gets new fraction data with matching denominator from the matchDenominator method
-		int finalNumerator=info[1]+info[2];//Adds the two numerators
-		Frac finalFrac = new Frac(finalNumerator,info[0]);//Creates
-		return finalFrac;
+public class Calculator {							//Class for performing mathematical operations to fractions
+	Frac add(Frac f1, Frac f2){						//Addition class
+		int[] info=new int[3];						//Creates an array in order to receive data from the matchDenominator method
+		System.arraycopy(matchDenominator(f1,f2),0,info,0,3);	//Gets new fraction data with matching denominator from the matchDenominator method
+		int finalNumerator=info[1]+info[2];			//Adds the two numerators
+		Frac finalFrac = new Frac(finalNumerator,info[0]);		//Creates a Frac object containing the sum of the two fractions
+		return finalFrac;							//Returns final Frac object
 	}
-	Frac subtract(Frac f1, Frac f2){
-		int[] info=new int[3];
-		System.arraycopy(matchDenominator(f1,f2),0,info,0,3);
-		int finalNumerator=info[1]-info[2];
-		Frac finalFrac = new Frac(finalNumerator,info[0]);
-		return finalFrac;
+	Frac subtract(Frac f1, Frac f2){				//Subtraction class
+		int[] info=new int[3];						//Creates an array in order to receive data from the matchDenominator method
+		System.arraycopy(matchDenominator(f1,f2),0,info,0,3);	//Gets new fraction data with matching denominator from the matchDenominator method
+		int finalNumerator=info[1]-info[2];			//Subtracts the two numerators
+		Frac finalFrac = new Frac(finalNumerator,info[0]);		//Creates a Frac object containing the difference of the two fractions
+		return finalFrac;							//Returns final Frac object
 	}
 
-	Frac multiply(Frac f1, Frac f2){
-		Frac finalFrac = new Frac(f1.getNumerator()*f2.getNumerator(),f1.getDenominator()*f2.getDenominator());
-		return finalFrac;
+	Frac multiply(Frac f1, Frac f2){				//Multiplication class
+		Frac finalFrac = new Frac(f1.getNumerator()*f2.getNumerator(),f1.getDenominator()*f2.getDenominator());	//Multiplies the numerators and denominators of the two fractions
+		return finalFrac;							//Returns final Frac object
 	}
-	Frac divide(Frac f1, Frac f2){
-		Frac finalFrac = new Frac(f1.getNumerator()*f2.getDenominator(),f1.getDenominator()*f2.getNumerator());
-		return finalFrac;
+	Frac divide(Frac f1, Frac f2){					//Division class
+		Frac finalFrac = new Frac(f1.getNumerator()*f2.getDenominator(),f1.getDenominator()*f2.getNumerator());	//Divides the two fractions
+		return finalFrac;							//Returns the final Frac object
 	}
-	private int gcd(int n1, int n2){
-		while (n2>0){
+	private int gcd(int n1, int n2){				//Class for finding greatest common denominator of two ints
+		while (n2>0){								//Loop for finding GCD
 			int placeholder=n2;
 			n2=n1%n2;
 			n1=placeholder;
 		}
-		return n1;
+		return n1;									//Returns GCD as integer
 	}
-	private int lcm(int n1, int n2){
-		int lcmresult= n1*(n2/gcd(n1,n2));
-		return lcmresult;
+	private int lcm(int n1, int n2){				//Class for finding LCM of two integers
+		int lcmresult= n1*(n2/gcd(n1,n2));			//Finds LCM using the GCD method
+		return lcmresult;							//Returns LCM as integer
 	}
-	int[] matchDenominator(Frac f1, Frac f2){
-		int[] infoArray=new int[3];
-		infoArray[0]=lcm(f1.getDenominator(),f2.getDenominator());
-		int multiple1=infoArray[0]/f1.getDenominator();
-		int multiple2=infoArray[0]/f2.getDenominator();
-		infoArray[1]=multiple1*f1.getNumerator();
-		infoArray[2]=multiple2*f2.getNumerator();
-		return infoArray;
+	int[] matchDenominator(Frac f1, Frac f2){		//Class for setting the denominators of two fracions equal
+		int[] infoArray=new int[3];					//Array for holding shared denominator and the new numerators for each fraction
+		infoArray[0]=lcm(f1.getDenominator(),f2.getDenominator());	//Finds common denominator by finding LCM between the two original denominators
+		int multiple1=infoArray[0]/f1.getDenominator();	//Finds the multiple needed for converting the numerator of the first fraction
+		int multiple2=infoArray[0]/f2.getDenominator();	//Finds the multiple needed for converting the numerator of the second fraction
+		infoArray[1]=multiple1*f1.getNumerator();		//Multiplies original numerator of fraction 1 by the multiple in order to find the new numerator
+		infoArray[2]=multiple2*f2.getNumerator();		//Multiplies original numerator of fraction 2 by the multiple in order to find the new numerator
+		return infoArray;							//Returns new denominator and two new numerators in an array
 	}
-	Frac simplify(Frac frac){
-		int multiple=gcd(frac.getNumerator(),frac.getDenominator());
-		int finalNumerator=frac.getNumerator()/multiple;
-		int finalDenominator=frac.getDenominator()/multiple;
+	Frac simplify(Frac frac){						//Class for simplifying fractions
+		int multiple=gcd(frac.getNumerator(),frac.getDenominator());	//Finds gcd between numerator and denominator
+		int finalNumerator=frac.getNumerator()/multiple;				//Divides both numerator by GCD
+		int finalDenominator=frac.getDenominator()/multiple;			//Divides denominator by GCD
 		Frac finalFrac=new Frac(finalNumerator,finalDenominator);
 		return finalFrac;
 	}
--- src/MainCalc.java
+++ src/MainCalc.java
@@ -1,67 +1,67 @@
 //By Ryan Chan
 //Fraction Calculator
 //CDM AP Computer Science, 2015
-import java.util.Scanner; //Imports the Scanner class, used for user input
+import java.util.Scanner; 								//Imports the Scanner class, used for user input
 
 public class MainCalc {
-	public static void main(String[] args){ //Main method
-		Calculator calc = new Calculator(); //Creates new Calculator object
+	public static void main(String[] args){ 			//Main method
+		Calculator calc = new Calculator(); 			//Creates new Calculator object
 		
 		System.out.println("Welcome to Frac Calc, by Ryan Chan. Type /help for info on usage");		
 		System.out.println("Please enter your statement: ");
-		FracPair pair=inputFrac(); //Executes the inputFrac method, which grabs user input and filters it. Data is returned in the form of a FracPair object, which holds two fractions and an operation
-		Frac resultFrac = new Frac(0,0);//Initializes a Frac object, to hold the resulting fraction.
+		FracPair pair=inputFrac(); 						//Executes the inputFrac method, which grabs user input and filters it. Data is returned in the form of a FracPair object, which holds two fractions and an operation
+		Frac resultFrac = new Frac(0,0);				//Initializes a Frac object, to hold the resulting fraction.
 		
-		if (pair.operation==1){ //Adds the two Frac objects returned by inputFrac()
+		if (pair.operation==1){ 						//Adds the two Frac objects returned by inputFrac()
 			resultFrac=calc.add(pair.f1,pair.f2);
 		}
-		else if (pair.operation==2){//Finds the difference between the two Frac objects returned by inputFrac()
+		else if (pair.operation==2){					//Finds the difference between the two Frac objects returned by inputFrac()
 			resultFrac=calc.subtract(pair.f1,pair.f2);
 		}
-		else if (pair.operation==3){//Multiplies the two Frac objects returned by inputFrac()
+		else if (pair.operation==3){					//Multiplies the two Frac objects returned by inputFrac()
 			resultFrac=calc.multiply(pair.f1,pair.f2);
 		}
-		else if (pair.operation==4){//Divides the two Frac objects returned by inputFrac()
+		else if (pair.operation==4){					//Divides the two Frac objects returned by inputFrac()
 			resultFrac=calc.divide(pair.f1,pair.f2);
 		}
 		/*else if (pair.operation==5){
 			System.out.println("a");
 			break;
 		*/
-		Frac finalFrac = calc.simplify(resultFrac); //Simplifies the resulting Fraction
-		System.out.println("Your answer is: "+finalFrac.getString());//Prints out final answer
-		}
+		Frac finalFrac = calc.simplify(resultFrac); 	//Simplifies the resulting Fraction
+		System.out.println("Your answer is: "+finalFrac.getString());	//Prints out final answer
+	}
 	static FracPair inputFrac(){
-		Scanner sc = new Scanner(System.in);//Initializes new Scanner
-		String input = sc.nextLine();//Grabs user input
-		sc.close();//Closes scanner object
-		int operation = 0; //Represents which operation will be used. 1=addition, 2=subtraction, 3=multiplication, 4=division
+		Scanner sc = new Scanner(System.in);			//Initializes new Scanner
+		String input = sc.nextLine();					//Grabs user input
+		sc.close();										//Closes scanner object
+		int operation = 0; 								//Represents which operation will be used. 1=addition, 2=subtraction, 3=multiplication, 4=division
 		
 			
 		//Searches for the number of '/' in the input string, in order to determine if divison will be used
-		int indexCount=0; //Initializes variable for counting the number of "/"
-		int index = input.indexOf("/"); //.indexOf will return -1 if no "/" is found. If not, it will return the placement of the next "/
+		int indexCount=0; 								//Initializes variable for counting the number of "/"
+		int index = input.indexOf("/"); 				//.indexOf will return -1 if no "/" is found. If not, it will return the placement of the next "/
 		while (index != -1){
-			index = input.indexOf("/",index); //Searches for the next instance of "/", starting at the place of the previous "/"
-			indexCount ++; //Increases the index count by 1
+			index = input.indexOf("/",index); 			//Searches for the next instance of "/", starting at the place of the previous "/"
+			indexCount ++; 								//Increases the index count by 1
 			if(index!=-1){
-				index +=1;//Increases the index by 1, so that .indexOf will return the next "/"
+				index +=1;								//Increases the index by 1, so that .indexOf will return the next "/"
 			}
 		}
 		
-		if (input.indexOf("+")!=-1){ //If the input string contains a "+", set operation to addition
+		if (input.indexOf("+")!=-1){ 					//If the input string contains a "+", set operation to addition
 			operation=1;
 		}
-		else if (input.indexOf("-")!=-1){//If the input string contains a "-", set operation to subtraction
+		else if (input.indexOf("-")!=-1){				//If the input string contains a "-", set operation to subtraction
 			operation=2;
 		}
-		else if (input.indexOf("*")!=-1){//If the input string contains a "*", set operation to multiplication
+		else if (input.indexOf("*")!=-1){				//If the input string contains a "*", set operation to multiplication
 			operation=3;
 		}
-		else if (indexCount>=3){//If number of "/" in the String is greater than or equal to 3, then operation is set to division
+		else if (indexCount>=3){						//If number of "/" in the String is greater than or equal to 3, then operation is set to division
 			operation=4;
 		}
-		else if (input.indexOf("/help")!=-1){ //If input contains /help, print out this
+		else if (input.indexOf("/help")!=-1){ 			//If input contains /help, print out this
 			System.out.println("To use, type two fractions seperated by an operator");
 			System.out.println("Fractions are represented as [numerator]/[denominator]");
 			System.out.println("Valid operators are +,-,*,/");
@@ -69,28 +69,28 @@ public class MainCalc {
 			System.out.println("\t1/2 + 1/3");
 			System.out.println("Will result in an answer of 5/6");
 			FracPair pair = new FracPair(null,null,5);
-			return pair; //Returns an empty FracPair with operation 5
+			return pair; 								//Returns an empty FracPair with operation 5
 		}
-		else { //If no operator is found, print error message and exit program
+		else { 											//If no operator is found, print error message and exit program
 			System.out.println("Invalid statement, operation not specified");
 			System.exit(0);
 		}
 		
-		input = input.replaceAll("\\s+", ""); //Removes all spaces in the input
+		input = input.replaceAll("\\s+", ""); 			//Removes all spaces in the input
 		
-		Scanner fracFilter = new Scanner(input);//Initialize new Scanner with the edited input string
-		fracFilter.useDelimiter("/|\\+|\\-|\\*"); //Sets "+","-","*",and "/" as delimiters, which serve to seperate sections of a string.
-		int numerator1,numerator2,denominator1,denominator2; //Initializes integers for holding data
+		Scanner fracFilter = new Scanner(input);		//Initialize new Scanner with the edited input string
+		fracFilter.useDelimiter("/|\\+|\\-|\\*"); 		//Sets "+","-","*",and "/" as delimiters, which serve to seperate sections of a string.
+		int numerator1,numerator2,denominator1,denominator2; 	//Initializes integers for holding data
 		
-		numerator1=fracFilter.nextInt(); //Takes next integer as numerator 1
-		denominator1=fracFilter.nextInt(); //Takes next integer as denominator 1
-		numerator2=fracFilter.nextInt(); //Takes next integer as numerator 2
-		denominator2=fracFilter.nextInt(); //Takes next integer as denominator 2
-		fracFilter.close();//Closes scanner object
+		numerator1=fracFilter.nextInt(); 				//Takes next integer as numerator 1
+		denominator1=fracFilter.nextInt(); 				//Takes next integer as denominator 1
+		numerator2=fracFilter.nextInt(); 				//Takes next integer as numerator 2
+		denominator2=fracFilter.nextInt(); 				//Takes next integer as denominator 2
+		fracFilter.close();								//Closes scanner object
 
-		Frac frac1 = new Frac(numerator1,denominator1); //Creates two Frac objects with the numerators and denominators
+		Frac frac1 = new Frac(numerator1,denominator1);	//Creates two Frac objects with the numerators and denominators
 		Frac frac2 = new Frac(numerator2,denominator2);
-		FracPair pair = new FracPair(frac1,frac2,operation); //Creates a FracPair object that holds the two Frac objects and an integer to signal operation
-		return pair; //Returns the FracPair 'pair'
+		FracPair pair = new FracPair(frac1,frac2,operation);	//Creates a FracPair object that holds the two Frac objects and an integer to signal operation
+		return pair; 									//Returns the FracPair 'pair'
 	}
 }
