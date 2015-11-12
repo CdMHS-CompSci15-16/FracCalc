14df748ff8c07768370f0142ff2e753c07290c75
NotRyan <ryanchan989@gmail.com>
2015-11-11 21:10:06 -0800

    1.2.0

--- src/mainCalc.java
+++ src/mainCalc.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class mainCalc {
+	final static boolean SHOW_PROCESS=true;	//Toggle this to have the calculator show its process for solving larger expressions
 	public static void main(String[] args) {
 		System.out.println("Welcome to Fraction Calculator!");
 		System.out.println("Written by Ryan Chan, by CDM AP Computer Science 2015");
-		System.out.println("Standard edition");
+		System.out.println("Extra-Credit edition");
 		Scanner sc = new Scanner(System.in);
 		String input;
 		boolean continueLoop = true;
@@ -15,10 +16,62 @@ public class Calculator {
 				continueLoop = false;
 				System.out.println("Thank you for using Fraction Calculator, have a nice day!");
 			} else {
-				System.out.println(operate(input)); //Calls the operate method, which will perform all necessary operations within the string. This value is then printed
+				System.out.println(parse(input)); //Calls the parse method, which will use order of operations to perform all necessary operations within the string. This value is then printed
 			}
 		}
 		sc.close();
+	}
+	public static String parse(String input) { //Determines order of operation and executes operations accordingly
+		int operatorPos = 0;
+		int startPos = 0;
+		int endPos = 0;
+		String excerpt, firstHalf, secondHalf, combined = null;
+		while (input.indexOf(" ") != -1) { //Loops until there is only one term in the string, which will be the final answer
+			if ((input.indexOf("*") != -1)||(input.indexOf("/ ") != -1)) {	//Searches for multiplication and division first
+				if ((input.indexOf("*")!=-1)&&input.indexOf("/ ")!=-1){	//Finds first instance of multiplication or division, if present
+					operatorPos = Math.min(input.indexOf("*"),input.indexOf("/ "));
+				} else if ((input.indexOf("*")!=-1)&&input.indexOf("/ ")==-1){
+					operatorPos = input.indexOf("*");
+				} else if ((input.indexOf("*")==-1)&&input.indexOf("/ ")!=-1){
+					operatorPos=input.indexOf("/ ");
+				}
+			} else if ((input.indexOf("+") != -1)||(input.indexOf("- ") != -1)) {	//Searches for addition and subtraction if no multiplication or division remain
+				if ((input.indexOf("+")!=-1)&&input.indexOf("- ")!=-1){	//Finds first instance of addition or subtration, if present
+					operatorPos = Math.min(input.indexOf("+"),input.indexOf("- "));
+				} else if ((input.indexOf("+")!=-1)&&input.indexOf("- ")==-1){
+					operatorPos = input.indexOf("+");
+				} else if ((input.indexOf("+")==-1)&&input.indexOf("- ")!=-1){
+					operatorPos=input.indexOf("- ");
+				}
+			}
+
+			if (input.lastIndexOf(" ", operatorPos - 2) != -1) {	//Checks if the number before the operator is the first in the string and records starting position
+				startPos = input.lastIndexOf(" ", operatorPos - 2) + 1;	
+			} else {
+				startPos = 0;
+			}
+			if (input.indexOf(" ", operatorPos + 2) != -1) {	//Checks if the number after the operator is the last number and records end position
+				endPos = input.indexOf(" ", operatorPos + 2);
+			} else {
+				endPos = input.length();
+			}
+
+			if (SHOW_PROCESS) {
+				System.out.println("Now Operating: \t[" + input.substring(startPos, endPos) + "]");
+			}
+
+			firstHalf = input.substring(0, startPos);	//First half of string taken up until the starting point for the operation
+			secondHalf = input.substring(endPos);	//Second half of string taken from ending point of the operation to string ending
+			excerpt = input.substring(startPos, endPos);	//Operation string is taken between starting point and ending point
+
+			combined = operate(excerpt);	//Operates on the excerpt
+
+			input = firstHalf + combined + secondHalf;
+			if (SHOW_PROCESS) {
+				System.out.println("New String: \t" + input);
+			}
+		}
+		return combined;
 	}
 	public static String operate(String input) {
 		//Initializes variables for two fractions. Each fraction has a variable for its coefficient, numerator, and denominator
@@ -71,6 +124,7 @@ public class Calculator {
 			temp = input.substring(operatorPos + 2,  underscorePos);
 			f2c=(Integer.parseInt(temp));
 		}
+		
 		if (input.indexOf("/",  operatorPos + 1) != -1) {	//If the second term contains a "/", and is therefore a fraction, finds the numerator and denominator
 			slashPos = input.indexOf("/",  operatorPos + 1);	//Finds position of "/"
 			if (input.indexOf("_",  operatorPos) != -1) {	//If the fraction has a coefficient, numerator is taken as the string between "_" and "/"
@@ -78,20 +132,18 @@ public class Calculator {
 			} else {
 				temp = input.substring(operatorPos + 2,  slashPos);	//If there is no coefficient, string is taken from operator index to the "/"
 			}
-			f2n=(Integer.parseInt(temp));
-			
+			f2n=(Integer.parseInt(temp));		
 			temp = input.substring(slashPos + 1);	//Denominator is taken as the string between "/" and operator
 			f2d=(Integer.parseInt(temp));
 		} else {
 			temp = input.substring(operatorPos + 2);	//If there is no fraction, then the whole number is stored as a coefficient
 			f2c=(Integer.parseInt(temp));
-		}
+		}		
 		//Converts fraction from a mixed number to an inproper fraction
 		f1n = f1n + (f1c * f1d);
 		f1c = 0;
 		f2n = f2n + (f2c * f2d);
 		f2c = 0;
-		
 		if (input.indexOf("+") != -1) {	//Calls relevant arithmetic method depending on the operator
 			result = add(f1n,f1d,f2n,f2d);
 		} else if (input.indexOf("- ") != -1) {
@@ -102,6 +154,9 @@ public class Calculator {
 			result = divide(f1n,f1d,f2n,f2d);
 		} else {
 			System.out.println("Error! Operator not present");
+		}
+		if (f1d==0||f2d==0){
+			result = "Error:Divided_By_Zero";
 		}
 		return result;
 	}
@@ -132,7 +187,7 @@ public class Calculator {
 			result = "Error:Divide_By_Zero";
 		} else {
 			int f3n=f1n*f2d;//Multiplies the first fraction by the inverse of the second
-			int f3d=f1d*f2n;		
+			int f3d=f1d*f2n;
 			result = convertToMixed(f3n,f3d);	//Converts the resulting fraction to a mixed number
 		}
 		return result;
@@ -167,16 +222,16 @@ public class Calculator {
 			return 1241412; //This is just for debugging; it should never actually happen
 		}
 	}
-	public static int gcf(int n1, int n2){				//Class for finding greatest common denominator of two ints
-		while (n2>0){								//Loop for finding GCD
-			int temp=n2;
-			n2=n1%n2;
-			n1=temp;
+	public static int gcf(int num1, int num2){				//Class for finding greatest common denominator of two ints
+		while (num2>0){								//Loop for finding GCF
+			int temp=num2;
+			num2=num1%num2;
+			num1=temp;
 		}
-		return n1;									//Returns GCD as integer
+		return num1;									//Returns GCF as integer
 	}
 	public static int lcm(int n1, int n2){				//Class for finding LCM of two integers
-		int lcmResult= n1*(n2/gcf(n1,n2));			//Finds LCM using the GCD method
+		int lcmResult= (n2/gcf(n1,n2)*n1);			//Finds LCM using the GCF method
 		return lcmResult;							//Returns LCM as integer
 	}
 	public static int matchDenominator(int f1n, int f1d, int f2n, int f2d, String placeToReturn){	//Matches the denominators of two fractions by multiplying by constants, then the requested number is returned
